3fd525d966083674749ccab125213ef5d4e6298a
Lindsay Warr <iamalion@gmail.com>
2023-06-23 09:56:40 -0700

    Refactor tests (add beforeEach)

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,20 +1,22 @@
 import { Person } from './../src/person.js'
 
 describe('Person  ', () => {
+    let person;
+
+    beforeEach(() => {
+        person = new Person("Lindsay", 36)
+    });
 
     test('should create a person object with an Earth age property', () => {
-        const person = new Person("Lindsay",36);
         expect(person.firstName).toBe("Lindsay");
         expect(person.earthAge).toEqual(36);
     });
 
     test('should calculate the age of the person in Mercury, Venus, Mars, and Jupiter years', () => {
-        const person = new Person ("Lindsay", 36);
         expect(person.calculateAge("Mercury")).toEqual(8.64)
     })
 
     test('should return an invalid response if an invalid planet is entered', () => {
-        const person = new Person ("Lindsay", 36);
         expect(person.calculateAge("Pluto")).toBe("Invalid planet!");
 
     })
