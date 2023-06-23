2d00afbe7bd50fcbb13900c0d6d664662760fdc8
Lindsay Warr <iamalion@gmail.com>
2023-06-23 09:48:05 -0700

    Pass second test

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -3,12 +3,13 @@ import { Person } from './../src/person.js'
 describe('Person  ', () => {
 
     test('should create a person object with an Earth age property', () => {
-      const person = new Person("Lindsay");
+      const person = new Person("Lindsay",36);
       expect(person.firstName).toBe("Lindsay");
+      expect(person.earthAge).toEqual(36);
     });
 
     test('should calculate the age of the person in Mercury, Venus, Mars, and Jupiter years', () => {
         const person = new Person ("Lindsay", 36);
-        expect(person.calculateAge()).toEqual(8.64)
+        expect(person.calculateAge("Mercury")).toEqual(8.64)
     })
 })
--- src/person.js
+++ src/person.js
@@ -6,7 +6,16 @@ export class Person {
     calculateAge(planet){
         let planetAge;
         if (planet === "Mercury"){
-            planetAge = this.earthAge * 0.24
+            planetAge = this.earthAge * 0.24;
+            return planetAge;
+        } else if (planet === "Venus") {
+            planetAge = this.earthAge * 0.62;
+            return planetAge;
+        } else if (planet === "Mars"){
+            planetAge = this.earthAge * 1.88;
+            return planetAge;
+        } else if (planet === "Jupiter"){
+            planetAge = this.earthAge * 11.86;
             return planetAge;
         }
 
