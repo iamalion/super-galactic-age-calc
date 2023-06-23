8ac8c0cf790b4ff6165f3ca9af3a36bad05ae6ec
Lindsay Warr <iamalion@gmail.com>
2023-06-23 09:32:42 -0700

    Second good fail

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -7,4 +7,9 @@ describe('Person  ', () => {
       expect(person.firstName).toBe("Lindsay");
       expect(person.earthAge).toEqual(36);
     });
+
+    test('should calculate the age of the person in Mercury, Venus, Mars, and Jupiter years', () => {
+        const person = new Person ("Lindsay", 36);
+        expect(person.calculateAge()).toEqual(8.64)
+    })
 })
--- src/person.js
+++ src/person.js
@@ -2,6 +2,9 @@ export class Person {
     constructor(firstName, earthAge){
         this.firstName = firstName;
         this.earthAge = earthAge;
+    }
+
+    calculateAge(){
         
     }
 }
