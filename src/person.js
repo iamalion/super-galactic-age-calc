cef9ca96ca9c692fee324261f81c5bcc7ec79dcc
Lindsay Warr <iamalion@gmail.com>
2023-06-23 11:26:12 -0700

    Refactor (DRY) code, remove mult return statements

--- src/person.js
+++ src/person.js
@@ -24,20 +24,17 @@ export class Person {
         let yearsPassed;
         if (planet === "Mercury") {
             yearsPassed = ((this.earthAge - this.pastAge) * 0.24).toFixed(2);
-            return parseFloat(yearsPassed);
         } else if (planet === "Venus") {
             yearsPassed = ((this.earthAge - this.pastAge) * 0.62).toFixed(2);
-            return parseFloat(yearsPassed);
         } else if (planet === "Mars") {
             yearsPassed = ((this.earthAge - this.pastAge) * 1.88).toFixed(2);
-            return parseFloat(yearsPassed);
         } else if (planet === "Jupiter") {
             yearsPassed = ((this.earthAge - this.pastAge) * 11.86).toFixed(2);
-            return parseFloat(yearsPassed);
         } else {
             return "Invalid planet!"
         }
-            
+        return parseFloat(yearsPassed)
+          
     }
         
 }
